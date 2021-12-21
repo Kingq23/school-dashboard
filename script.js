74b9e63b0d441a68784fdeed1a89132766df99e4
Kingq23 <80791284+Kingq23@users.noreply.github.com>
2021-12-21 12:14:37 -0500

    add weather

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+function get_weather() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=newton&appid=82b48f87b2380bc1cdaa44aba7e25525")
+    .then(response => response.json())
+    .then(data => {
+    // do stuff with the data
+    const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+    document.getElementById("weather_data").src=icon
+    })
+    .catch(() => {
+    
+    });
+}
 window.addEventListener('load', (event) => {
   
     startTime()
